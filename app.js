dfacb1f826893f3e63b379f0069c5cefc2b2ce61
Anjithkumar-7 <anjithsuma@gmail.com>
2022-11-02 20:00:06 -0500

    Adde the gridbuild logic to the repo

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var aeroplaneRouter = require('./routes/aeroplane')
+var aeroplaneRouter = require('./routes/aeroplane');
+var gridRouter = require('./routes/gridbuild');
 var app = express();
 
 // view engine setup
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/aeroplane', aeroplaneRouter);
+app.use('/gridbuild',gridRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
